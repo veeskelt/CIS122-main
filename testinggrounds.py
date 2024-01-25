3ee44929c003350951850f75d57653ed6f1adb4d
MoltenLichQueen <xethaios@gmail.com>
2024-01-24 19:59:32 -0800

    reworded w3q1&2 to be more appropriate

--- testinggrounds.py
+++ testinggrounds.py
@@ -1,9 +1,1 @@
-print("this is a line")
-print("this is a line with a break \n")
-print("is this one 4 lines down?") #it is. a \n linebreak inside the quotes
-                                   #indicates you want a line break in
-                                   #addition to the one that the print
-                                   #function ships with by default.
-print(3**3**2)
-print(3**9)     #yup they're done right to left
-print(3**2**3)
+# empty
