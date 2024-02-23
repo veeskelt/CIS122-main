8734d9443f90c7d6dea0b385d0fee9b59ec4beab
MoltenLichQueen <xethaios@gmail.com>
2024-02-23 09:14:48 -0800

    practice7.py: fixing function comments, tidying up code a bit
    Signed-off-by: MoltenLichQueen <xethaios@gmail.com>

--- practice7.py
+++ practice7.py
@@ -4,7 +4,7 @@
 # Hi! Enter a range of integers, and I'll calculate the average of all the
 # numbers, print the highest and lowest of them, and tell you their sum and
 # average!
-# sum of the numbers!
+# list_sum of the numbers!
 # Enter your number: 4
 # Continue? y
 # Enter your number: 8
@@ -21,91 +21,81 @@ import valid as v
 
 
 def main():
-    """The main function of the program
-    returns: nothing"""
+    """
+    The main function of the program
+    :return: nothing
+    """
     cont = 'y'
     nums = []
-    sums = 0
+    list_sum = 0
     avg = 0.0
     high = 0
     low = 0
-    count = 0
+    prompt = "Do you want to continue? (y/n) "
     greeting()
-#    print(type(nums))
     while cont == 'y':
-        nums = input_nums(nums)
-        cont = proceed()
-    print(nums)
-    sums = sum_calc(nums)
-#    print(sums)
+        nums = user_input(nums)
+        cont = v.get_y_or_n(prompt)
+    list_sum = list_sum_calc(nums)
     high = high_eval(nums)
     low = low_eval(nums)
     avg = avg_calc(nums)
-    output(count, sums, avg, high, low)
+    output(nums, list_sum, avg, high, low)
 
 
 def greeting():
-    """Outputs a greeting to the user, and an explanation of the program.
-    returns: nothing"""
+    """
+    Outputs a greeting to the user, and an explanation of the program.
+    :return: nothing
+    """
     print("Hi! Enter a range of numbers, and I'll calculate the average of all"
           "the \nnumbers, print the highest and lowest of them, and tell you "
           "their sum and \naverage!")
 
 
-def input_nums(nums):
-    """Gets user input of a series of numbers, one after the other; stores
+def user_input(nums):
+    """
+    Gets user input of a series of numbers, one after the other; stores
     these in a float because the calculations are performed sequentially.
-    returns: usrNum, an int"""
+    :param nums: a list of numbers
+    :return: usrNum, an int
+    """
     nums.append(v.get_real())
     return nums
 
 
-def proceed():
-    """Prompts the user for a request to continue until a valid input is
-    entered ('y' or 'n').
-    returns: more, a char"""
-    # cont = ""
-    cont = input("Do you want to continue? (y/n) ")
-    cont = cont.lower()
-    # The following checks to make sure the input was a valid input
-    if cont != 'y' and cont != 'n':
-        print("This is not a valid input. Please try again.")
-        cont = input("Do you want to continue? (y/n) ")
-        cont = cont.lower()
-    return cont
-
-
-def sum_calc(a_list):
+def list_sum_calc(a_list):
     """
-    Calculates the sum of every value the user inputs.
-    param: a_list, any list of ints or floats
-    returns: total_sums, a float, the sum of every item in the list
+    Calculates the list_sum of every value the user inputs.
+    :param: a_list, any list of ints or floats
+    :return: total_list_sum, a float, the list_sum of every item in the list
     """
-    total_sum = 0.0
+    total_list_sum = 0.0
     for index in range(len(a_list)):
-        total_sum = total_sum + a_list[index]
-    return total_sum
+        total_list_sum = total_list_sum + a_list[index]
+    return total_list_sum
 
 
 def avg_calc(a_list):
-    """Calculates the average of all user inputs
-    param: nums, an int, the current user input
-    param: count, an int, keeps track of how many values have been input
-    returns: avg"""
+    """
+    Calculates the average of all user inputs
+    :param: a_list, any list of ints or floats
+    :return: avg, the average of the terms in a_list
+    """
     average = 0.0
-    avg_sum = sum_calc(a_list)
-    average = avg_sum / len(a_list)
+    avg_list_sum = list_sum_calc(a_list)
+    average = avg_list_sum / len(a_list)
     return average
 
 
 def high_eval(a_list):
     """
     Function to find the maximum value in a list
-    :param a_list: a list of values
+    :param: a_list, any list of ints or floats
     :return: the maximum value in the list
     """
     list_max = a_list[0]
-    for index in range(len(a_list)):
+    for index in a_list:
         if index > list_max:
             list_max = index
     return list_max
@@ -114,26 +104,28 @@ def high_eval(a_list):
 def low_eval(a_list):
     """
     Function to find the maximum value in a list
-    :param a_list: a list of values
+    :param: a_list, any list of ints or floats
     :return: the minimum value in the list
     """
     list_min = a_list[0]
-    for index in range(len(a_list)):
+    for index in a_list:
         if index < list_min:
             list_min = index
     return list_min
 
 
-def output(count, sums, avg, high, low):
-    """Prints the output of all the functions.
-    param: count, an int, keeps track of how many times the loop was run
-    param: sums, an int, the sum of the user inputs
-    param: avg, an int, the average of the user inputs
-    param: high, an int, the highest value of the user inputs
-    param: low, an int, the lowest value of the user inputs
-    returns: None"""
-    print("You entered", count, "numbers.")
-    print("The sum of these is", sums)
+def output(a_list, list_sum, avg, high, low):
+    """
+    Prints the output of all the functions.
+    :param a_list: any list of ints or floats
+    :param list_sum: an int, the sum of the user inputs
+    :param avg:, a float, the average of the user inputs
+    :param high:, a float, the highest value of the user inputs
+    :param low: a float, the lowest value of the user inputs
+    :returns None
+    """
+    print("You entered", len(a_list), "numbers")
+    print("The sum of these is", list_sum)
     print("The average of them is", avg)
     print("The lowest of these is", low)
     print("The highest of these is", high)
