471063bc3fb6a43a1b2eceef984a267f92847957
MoltenLichQueen <xethaios@gmail.com>
2024-03-16 14:57:30 -0700

    my_notes.py: initial submission, a file for things I won't remember

---
+++ my_notes.py
@@ -0,0 +1,5 @@
+def format_function_notes():
+    floating = 0.987654321
+    integer = 9
+    print("{:<5.2f} {:<5}".format(floating, integer))
+    # The format for justifying something you're rounding is justify.round
